53c87778ecf04768263eb9d0d193b78389813926
5675856 <163152652+5675856@users.noreply.github.com>
2024-05-22 14:46:16 +0300

    1

--- inter/comand.py
+++ inter/comand.py
@@ -1,6 +1,5 @@
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
-from django.core.management import call_command
-from django.core.wsgi import get_wsgi_application 
-application = get_wsgi_application()
-call_command('runserver',  '127.0.0.1:8000')
+import subprocess
+def runserver():
+    subprocess.run(['python', 'inter\manage.py', 'runserver'])
+if __name__ == '__main__':
+    runserver()

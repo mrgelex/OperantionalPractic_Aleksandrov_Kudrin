f81c4883033c5d00bb020251c886251231ba3a2b
5675856 <163152652+5675856@users.noreply.github.com>
2024-05-22 18:47:30 +0300

    bd.on.18.47

--- inter/main/models.py
+++ inter/main/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 class Devices(models.Model):
     id_device=models.IntegerField(max_length=3, primary_key=True)
-    serial=models.IntegerField(max_length=9)
-    imel=models.IntegerField(max_length=16)
+    serial=models.IntegerField(max_length=10)
+    IMEI=models.IntegerField(max_length=16)
     name=models.CharField(max_length=20)
     type=models.CharField(max_length=20)
 
     class Meta:
         managed = False
-        db_table = 'devices'
+        db_table = 'DEVICES'
 
     def __str__(self):
         return self.name
@@ -19,23 +19,23 @@ class Evet(models.Model):
     id_device=models.ForeignKey(Devices, on_delete=models.CASCADE)
     date_local=models.DateField()
     time_local=models.TimeField()
-    status=models.IntegerField(max_length=1)
+    status=models.IntegerField(max_length=5)
     depth=models.IntegerField(max_length=3)
     power=models.IntegerField(max_length=3)
 
     class Meta:
         managed = False
-        db_table = 'log_event'
+        db_table = 'LOG_EVENT'
 
 class Time(models.Model):
     id_log=models.IntegerField(primary_key=True)
     id_device=models.ForeignKey(Devices, on_delete=models.CASCADE)
     date_local=models.DateField()
     time_local=models.TimeField()
-    status=models.IntegerField(max_length=1)
+    status=models.IntegerField(max_length=5)
     depth=models.IntegerField(max_length=3)
     power=models.IntegerField(max_length=3)
 
     class Meta:
         managed = False
-        db_table = 'log_time'
+        db_table = 'LOG_TIME'
